db301d2d643d3298388445ef1d958dadec9efd1d
Manuel Maier <mm198@hdm-stuttgart.de>
2014-02-17 10:26:38 +0100

    Adjusted implementation to ezEngine API changes.

--- source/lcppCore/include/lcpp/core/schemeTypes.h
+++ source/lcppCore/include/lcpp/core/schemeTypes.h
@@ -155,17 +155,17 @@ namespace lcpp
     // namespace for all singletons such as bool types and void.
     namespace singletons
     {
-        extern LCPP_CORE_API const ezStatic<SchemeVoid> g_void;
-        extern LCPP_CORE_API const ezStatic<SchemeNil> g_nil;
-        extern LCPP_CORE_API const ezStatic<SchemeBool> g_true;
-        extern LCPP_CORE_API const ezStatic<SchemeBool> g_false;
+        extern LCPP_CORE_API const SchemeVoid g_void;
+        extern LCPP_CORE_API const SchemeNil g_nil;
+        extern LCPP_CORE_API const SchemeBool g_true;
+        extern LCPP_CORE_API const SchemeBool g_false;
     };
 
     // Easy access macros for all singleton instances
-    #define SCHEME_VOID  ::lcpp::singletons::g_void.GetStatic()
-    #define SCHEME_NIL   ::lcpp::singletons::g_nil.GetStatic()
-    #define SCHEME_TRUE  ::lcpp::singletons::g_true.GetStatic()
-    #define SCHEME_FALSE ::lcpp::singletons::g_false.GetStatic()
+    #define SCHEME_VOID  ::lcpp::singletons::g_void
+    #define SCHEME_NIL   ::lcpp::singletons::g_nil
+    #define SCHEME_TRUE  ::lcpp::singletons::g_true
+    #define SCHEME_FALSE ::lcpp::singletons::g_false
 
 #include "lcpp/core/implementation/schemeTypes_inl.h"
 }
