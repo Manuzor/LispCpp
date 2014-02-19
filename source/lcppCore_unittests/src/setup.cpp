a52553930ae645063a1fbab3e66149e508a6c724
Manuel Maier <mm198@hdm-stuttgart.de>
2014-02-19 08:49:12 +0100

    Renamed unittestModule.cpp -> setup.cpp

---
+++ source/lcppCore_unittests/src/setup.cpp
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+
+namespace lcpp { namespace unittests {
+
+    TEST_MODULE_INITIALIZE(SchemeTypeTests_Initialize)
+    {
+        ezStartup::StartupCore();
+    }
+
+    TEST_MODULE_CLEANUP(SchemeTypeTests_Cleanup)
+    {
+        ezStartup::ShutdownBase();
+    }
+
+}}
