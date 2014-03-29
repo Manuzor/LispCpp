4308b8d2199449ddd4c8a188b1d088c797da28ee
Manuel Maier <mjmaier@gmx.de>
2014-03-29 20:30:21 +0100

    Added unittest stub for SchemeSymbol.

---
+++ source/lcppCore_unittests/src/core/typeSystem/types/symbol.cpp
@@ -0,0 +1,38 @@
+﻿#include "stdafx.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace lcpp { namespace unittests {
+
+
+    TEST_CLASS(SchemeSymbolTests)
+    {
+    public:
+
+        TEST_METHOD(Type)
+        {
+            Assert::Fail(L"Not Implemented.");
+        }
+
+        TEST_METHOD(Construction)
+        {
+            Assert::Fail(L"Not Implemented.");
+        }
+
+        TEST_METHOD(Equality)
+        {
+            Assert::Fail(L"Not Implemented.");
+        }
+
+        TEST_METHOD(ConversionToBool)
+        {
+            Assert::Fail(L"Not Implemented.");
+        }
+
+        TEST_METHOD(ToString)
+        {
+            Assert::Fail(L"Not Implemented.");
+        }
+    };
+
+}}
