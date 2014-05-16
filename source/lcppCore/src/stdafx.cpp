62ded73d8ce4e68ea4d426d21199a027016f8be2
Manuel Maier <mjmaier@gmx.de>
2014-05-17 00:26:08 +0100

    Removed newdelete_inl.h.

--- source/lcppCore/src/stdafx.cpp
+++ source/lcppCore/src/stdafx.cpp
@@ -1,9 +1,1 @@
-﻿// stdafx.cpp : source file that includes just the standard includes
-// lcpp.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-#include "lcpp/foundation/implementation/newdelete_inl.h"
-
-// TODO: reference any additional headers you need in STDAFX.H
-// and not in this file
+﻿#include "stdafx.h"
