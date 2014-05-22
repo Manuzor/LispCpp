5e8187fdea859cfba98af76d5576c7ea7c668a77
Manuel Maier <mjmaier@gmx.de>
2014-05-22 11:43:51 +0100

    Implemented simple writer.

---
+++ source/lcppCore/include/lcpp/core/printer.h
@@ -0,0 +1,21 @@
+#pragma once
+
+namespace lcpp
+{
+    class SchemeObject;
+
+    class IPrinter
+    {
+    public:
+        virtual ~IPrinter();
+
+        virtual void print(SchemeObject& object) = 0;
+    };
+    
+    class Printer : public IPrinter
+    {
+    public:
+
+        virtual void print(SchemeObject& object) override;
+    };
+}
---
+++ source/lcppCore/src/lcpp/core/printer.cpp
@@ -0,0 +1,9 @@
+#include "stdafx.h"
+#include "lcpp/core/printer.h"
+#include "lcpp/core/typeSystem.h"
+#include <iostream>
+
+void lcpp::Printer::print(SchemeObject& object)
+{
+    std::cout << object.toString().GetData() << std::endl;
+}
