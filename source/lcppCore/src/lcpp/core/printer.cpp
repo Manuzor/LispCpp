6a7db5d8193942097586237a8772091489f68b9d
Manuel Maier <mjmaier@gmx.de>
2014-06-06 21:35:36 +0100

    Printer now also uses Ptr<> instead of ref.

--- source/lcppCore/src/lcpp/core/printer.cpp
+++ source/lcppCore/src/lcpp/core/printer.cpp
@@ -12,37 +12,40 @@ lcpp::Printer::~Printer()
 {
 }
 
-void lcpp::Printer::print(SchemeObject& object)
+void
+lcpp::Printer::print(Ptr<SchemeObject> object)
 {
-    if(object.is<SchemeVoid>())
-    {
-        return;
-    }
+    if(object->is<SchemeVoid>()){ return; }
 
-    *m_pOut << object.toString().GetData() << std::endl;
+    *m_pOut << object->toString().GetData() << std::endl;
 }
 
-void lcpp::Printer::print(const std::string& str)
-{
-    *m_pOut << str;
-}
-
-void lcpp::Printer::print(const ezString& str)
-{
-    print(str.GetData());
-}
-
-void lcpp::Printer::print(const ezStringBuilder& str)
-{
-    print(str.GetData());
-}
-
-void lcpp::Printer::print(const char* str)
+void
+lcpp::Printer::print(const std::string& str)
 {
     *m_pOut << str;
 }
 
-void lcpp::Printer::setOutputStream(std::ostream& out)
+void
+lcpp::Printer::print(const ezString& str)
+{
+    print(str.GetData());
+}
+
+void
+lcpp::Printer::print(const ezStringBuilder& str)
+{
+    print(str.GetData());
+}
+
+void
+lcpp::Printer::print(const char* str)
+{
+    *m_pOut << str;
+}
+
+void
+lcpp::Printer::setOutputStream(std::ostream& out)
 {
     m_pOut = &out;
 }
