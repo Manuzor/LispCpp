bf09bc297a589a0a5b55ef5ab13b2381b58290ea
Manuel Maier <mjmaier@gmx.de>
2014-06-11 10:17:23 +0100

    Some experiments in stdlib.lisp

--- data/base/stdlib.lisp
+++ data/base/stdlib.lisp
@@ -1,3 +1,22 @@
 
 (define x 1)
 (define y 2)
+
+(define eval-file
+    (lambda (fileName)
+        (eval
+            (read
+                ((lambda ()
+                    (define f (file-open fileName))
+                    (define content (file-read-string f))
+                    (file-close f)
+                    content
+                ))
+            )
+        )
+    )
+)
+
+;(eval-file "unittests.lisp")
+
+(define x (lambda (n) ((lambda () n))))
