2630ef18ac1272c7c0296521e6810db1539c48b5
Manuel Maier <mjmaier@gmx.de>
2014-07-22 08:58:17 +0100

    lib/tak.lisp no longer returns the tak function.

--- data/lib/tak.lisp
+++ data/lib/tak.lisp
@@ -5,8 +5,7 @@
     (print (format "call #{0}: {1} {2} {3}" counter x y z))
     (if (>= y x) ; y >= x
         z
-        (tak (tak (- x 1) y z)
-             (tak (- y 1) z x)
+          (tak (tak (- x 1) y z)
+               (tak (- y 1) z x)
              (tak (- z 1) x y))))
 (tak 4 1 0)
-tak
