bc5c0d52c63dd9356b2a5a3d56810bd1b445e744
Manuel Maier <mjmaier@gmx.de>
2014-07-25 22:30:28 +0100

    Implemented Stack, mainly tests.

--- source/lcppCoreCont/lcpp/core/containers/impl/stack.cpp
+++ source/lcppCoreCont/lcpp/core/containers/impl/stack.cpp
@@ -1,6 +1,13 @@
 #include "stdafx.h"
+#include "lcpp/core/containers/stack.h"
+#include "lcpp/core/typeSystem/types/nil.h"
+#include "lcpp/core/runtime.h"
 
 namespace lcpp
 {
-
+    Stack::Stack() :
+        StackBase(LispRuntime::instance()->allocator().get())
+    {
+        setNilElement(LCPP_NIL);
+    }
 }
