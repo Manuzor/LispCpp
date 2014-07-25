c5358783ec71f5cda0e79b4631a8cf28a7ed5b5f
Manuel Maier <mjmaier@gmx.de>
2014-07-25 17:36:07 +0100

    Added config.h to lcppCoreCont.

---
+++ source/lcppCoreCont/lcpp/core/config.h
@@ -0,0 +1,6 @@
+#ifdef LCPP_CORE_CONT_CONFIG_INCLUDED
+#error Config file is not supposed to be included more than once!
+#endif
+#define LCPP_CORE_CONT_CONFIG_INCLUDED
+
+#define LCPP_RUNTIME_TYPE_CHECK_ENABLED EZ_ON
