6362ce90bb08716d260f1c0d9bb4c980ab873214
Manuel Maier <mjmaier@gmx.de>
2014-07-25 22:33:48 +0100

    Some test for LispObjects in general.

--- source/lcppCoreCont_unittests/lcpp/core/typeSystem/test_object.cpp
+++ source/lcppCoreCont_unittests/lcpp/core/typeSystem/test_object.cpp
@@ -1,10 +1,14 @@
 #include "stdafx.h"
 #include "lcpp/core/typeSystem/object.h"
 #include "lcpp/core/typeSystem/types/nil.h"
-#include "lcpp/core/typeSystem/types/bool.h"
 
 LCPP_TEST_GROUP(Object);
 
 LCPP_TEST(Object, Basics)
 {
+    auto pObject = LCPP_NIL;
+
+    CUT_ASSERT.isTrue(pObject->isType(Type::Nil));
+    CUT_ASSERT.isTrue(pObject->getType() == Type(Type::Nil));
+    CUT_ASSERT.isTrue(pObject->getMetaInfo().getType() == pObject->getType());
 }
