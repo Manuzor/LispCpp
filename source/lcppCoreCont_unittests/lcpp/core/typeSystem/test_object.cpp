7f05c32b2fbb6d1d42d4763fa5eae3fa96e89657
Manuel Maier <mjmaier@gmx.de>
2014-07-29 23:01:38 +0100

    Upgraded lcpp.natvis

--- source/lcppCoreCont_unittests/lcpp/core/typeSystem/test_object.cpp
+++ source/lcppCoreCont_unittests/lcpp/core/typeSystem/test_object.cpp
@@ -12,3 +12,26 @@ LCPP_TestCase(Object, Basics)
     CUT_ASSERT.isTrue(pObject->getType() == Type(Type::Nil));
     CUT_ASSERT.isTrue(pObject->getMetaInfo().getType() == pObject->getType());
 }
+
+LCPP_TestCase(Object, AllTypes)
+{
+    auto pNil = LCPP_pNil;
+    auto pTrue = LCPP_pTrue;
+    auto pFalse = LCPP_pFalse;
+    auto pVoid = LCPP_pVoid;
+
+    auto pInteger = number::create(42);
+    auto pFloat = number::create(3.1415f);
+
+    auto pSymbol = symbol::create("hello-world");
+    // auto pString = string::create("this-is-a-string");
+
+    // auto pCons = cons::create(pInteger, pNil);
+    // auto pLambda = lambda::createBuiltin(pEnv, &lambda::builtin::myLambda);
+    // auto pSyntax = syntax::createBuiltin(&syntax::builtin::mySyntax);
+    auto pEnv = env::createTopLevel(pSymbol);
+
+    // auto pFile = file::create("the-file-name.txt");
+
+    return;
+}
