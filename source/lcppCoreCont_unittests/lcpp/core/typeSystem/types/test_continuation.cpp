6ef9f0e84c34036c916c37d0116a58e8f3b9ca2b
Manuel Maier <mjmaier@gmx.de>
2014-07-30 00:17:07 +0100

    Implemented continuation unit test.

--- source/lcppCoreCont_unittests/lcpp/core/typeSystem/types/test_continuation.cpp
+++ source/lcppCoreCont_unittests/lcpp/core/typeSystem/types/test_continuation.cpp
@@ -7,13 +7,32 @@
 
 namespace lcpp
 {
-    static Ptr<LispObject> sorter(Ptr<LispObject> pCont)
+    static auto g_sum = number::Integer_t(0);
+
+    static Ptr<LispObject> accumulator(Ptr<LispObject> pCont)
     {
-        return cont::getParent(pCont);
+        auto& stack = cont::getStack(pCont);
+
+        auto pToGo = stack.get(-1);
+        auto value = number::getInteger(pToGo);
+
+        if(value <= 0)
+        {
+            return cont::getParent(pCont);
+        }
+
+        ++g_sum;
+
+        stack.pop();
+        stack.push(number::create(value - 1));
+
+        return pCont;
     }
 
     static Ptr<LispObject> entry(Ptr<LispObject> pCont)
     {
+        CUT_ASSERT.isTrue(g_sum == 3);
+
         return cont::getParent(pCont);
     }
 }
@@ -26,8 +45,12 @@ LCPP_TestCase(Continuation, Basics)
     auto pInteger_2 = number::create(2);
     auto pInteger_3 = number::create(3);
 
-    auto pRootCont = cont::createTopLevel(&entry);
-    auto pSorterCont = cont::create(pRootCont, &entry);
+    auto pContRoot = cont::createTopLevel(&entry);
+    auto pContChild = cont::create(pContRoot, &accumulator);
 
-    cont::trampoline(pSorterCont);
+    cont::getStack(pContChild).push(number::create(3)); // Do 3 'iterations'
+
+    g_sum = 0;
+
+    cont::trampoline(pContChild);
 }
