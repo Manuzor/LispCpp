c6e4912fc324d42127bdc9018b4d998798635aa3
Manuel Maier <mjmaier@gmx.de>
2014-08-06 19:17:51 +0100

    Added test for custom stream for printer test.

--- source/lcppCoreCont_unittests/lcpp/core/test_printer.cpp
+++ source/lcppCoreCont_unittests/lcpp/core/test_printer.cpp
@@ -4,10 +4,35 @@
 #include "lcpp/core/typeSystem/types/continuation.h"
 #include "lcpp/core/typeSystem/types/void.h"
 #include "lcpp/core/typeSystem/types/number.h"
+#include "lcpp/core/runtime.h"
+
+namespace lcpp
+{
+    class MyStringStream :
+        public ezStreamWriterBase
+    {
+    public:
+
+        ezStringBuilder m_content;
+
+        virtual ezResult WriteBytes(const void* pWriteBuffer, ezUInt64 uiBytesToWrite) override
+        {
+            auto pBuffer = reinterpret_cast<const ezUInt8*>(pWriteBuffer);
+
+            for(auto i = ezUInt64(0); i < uiBytesToWrite; ++i)
+            {
+                m_content.Append(pBuffer[i]);
+            }
+
+            return EZ_SUCCESS;
+        }
+
+    };
+}
 
 LCPP_TestGroup(Printer);
 
-LCPP_TestCase(Printer, Basics)
+LCPP_TestCase(Printer, StandardOutput)
 {
     auto pContMain = cont::createTopLevel(LCPP_test_pRuntimeState);
     auto pContCall = cont::create(pContMain, &printer::print);
@@ -18,4 +43,25 @@ LCPP_TestCase(Printer, Basics)
     auto pResult = cont::getStack(pContMain)->get(-1);
 
     CUT_ASSERT.isTrue(isVoid(pResult));
+}
+
+LCPP_TestCase(Printer, CustomStream)
+{
+    auto stream = MyStringStream();
+    auto pState = LCPP_test_pRuntimeState;
+    pState->getPrinterState()->m_pOutStream = &stream;
+
+    auto pContMain = cont::createTopLevel(pState);
+    auto pContCall = cont::create(pContMain, &printer::print);
+    cont::getStack(pContCall)->push(number::create(42));
+
+    CUT_ASSERT.isTrue(stream.m_content.IsEmpty());
+
+    cont::trampoline(pContCall);
+
+    auto pResult = cont::getStack(pContMain)->get(-1);
+
+    CUT_ASSERT.isTrue(isVoid(pResult));
+
+    CUT_ASSERT.isTrue(stream.m_content.IsEqual("42"));
 }
