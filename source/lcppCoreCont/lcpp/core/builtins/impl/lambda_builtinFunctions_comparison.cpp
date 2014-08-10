fc457c043974d8146bcc5b44fe52b36416770ea1
Manuel Maier <mjmaier@gmx.de>
2014-08-10 10:00:29 +0100

    Split builtin lambdas into separate files for arithmetic and comparison functions.

---
+++ source/lcppCoreCont/lcpp/core/builtins/impl/lambda_builtinFunctions_comparison.cpp
@@ -0,0 +1,112 @@
+#include "stdafx.h"
+#include "lcpp/core/builtins/lambda_builtinFunctions.h"
+#include "lcpp/core/typeSystem/types/continuation.h"
+#include "lcpp/core/typeSystem/types/nil.h"
+#include "lcpp/core/typeSystem/types/number.h"
+#include "lcpp/core/typeSystem/types/bool.h"
+
+#include "lcpp/core/typeSystem/type.h"
+#include "lcpp/core/typeSystem/typeCheck.h"
+#include "lcpp/core/typeSystem/object.h"
+
+#define LCPP_lambda_builtinFunctions_defineComparisonFunctionHelperContent(operatorFunction) \
+    typeCheck(pCont, Type::Continuation);                                                    \
+    auto pStack = cont::getStack(pCont);                                                     \
+    auto& index = cont::getUserData(pCont);                                                  \
+                                                                                             \
+    const auto maxIndex = pStack->size() - 2;                                                \
+                                                                                             \
+    auto& pResult = pStack->get(-1);                                                         \
+                                                                                             \
+    if(index >= maxIndex)                                                                    \
+    {                                                                                        \
+        LCPP_cont_return(pCont, pResult);                                                    \
+    }                                                                                        \
+                                                                                             \
+    auto pLhs = pStack->get(index);                                                          \
+    ++index;                                                                                 \
+    auto pRhs = pStack->get(index);                                                          \
+                                                                                             \
+    pResult = operatorFunction(pLhs, pRhs);                                                  \
+                                                                                             \
+    LCPP_cont_tailCall(pCont)
+
+namespace lcpp
+{
+    namespace lambda
+    {
+        namespace builtin
+        {
+            Ptr<LispObject> greaterThan(Ptr<LispObject> pCont)
+            {
+                typeCheck(pCont, Type::Continuation);
+                auto pStack = cont::getStack(pCont);
+                cont::setUserData(pCont, 0);
+                pStack->push(LCPP_pFalse);
+                LCPP_cont_tailCall(pCont, &detail::greaterThan_helper);
+            }
+
+            Ptr<LispObject> detail::greaterThan_helper(Ptr<LispObject> pCont)
+            {
+                LCPP_lambda_builtinFunctions_defineComparisonFunctionHelperContent(number::greaterThan);
+            }
+
+            Ptr<LispObject> greaterThanOrEqual(Ptr<LispObject> pCont)
+            {
+                typeCheck(pCont, Type::Continuation);
+                auto pStack = cont::getStack(pCont);
+                cont::setUserData(pCont, 0);
+                pStack->push(LCPP_pFalse);
+                LCPP_cont_tailCall(pCont, &detail::greaterThanOrEqual_helper);
+            }
+
+            Ptr<LispObject> detail::greaterThanOrEqual_helper(Ptr<LispObject> pCont)
+            {
+                LCPP_lambda_builtinFunctions_defineComparisonFunctionHelperContent(number::greaterThanOrEqual);
+            }
+
+            Ptr<LispObject> equal(Ptr<LispObject> pCont)
+            {
+                typeCheck(pCont, Type::Continuation);
+                auto pStack = cont::getStack(pCont);
+                cont::setUserData(pCont, 0);
+                pStack->push(LCPP_pFalse);
+                LCPP_cont_tailCall(pCont, &detail::equal_helper);
+            }
+
+            Ptr<LispObject> detail::equal_helper(Ptr<LispObject> pCont)
+            {
+                LCPP_lambda_builtinFunctions_defineComparisonFunctionHelperContent(number::equal);
+            }
+
+            Ptr<LispObject> lowerThan(Ptr<LispObject> pCont)
+            {
+                typeCheck(pCont, Type::Continuation);
+                auto pStack = cont::getStack(pCont);
+                cont::setUserData(pCont, 0);
+                pStack->push(LCPP_pFalse);
+                LCPP_cont_tailCall(pCont, &detail::lowerThan_helper);
+            }
+
+            Ptr<LispObject> detail::lowerThan_helper(Ptr<LispObject> pCont)
+            {
+                LCPP_lambda_builtinFunctions_defineComparisonFunctionHelperContent(number::lowerThan);
+            }
+
+            Ptr<LispObject> lowerThanOrEqual(Ptr<LispObject> pCont)
+            {
+                typeCheck(pCont, Type::Continuation);
+                auto pStack = cont::getStack(pCont);
+                cont::setUserData(pCont, 0);
+                pStack->push(LCPP_pFalse);
+                LCPP_cont_tailCall(pCont, &detail::lowerThanOrEqual_helper);
+            }
+            Ptr<LispObject> detail::lowerThanOrEqual_helper(Ptr<LispObject> pCont)
+            {
+                LCPP_lambda_builtinFunctions_defineComparisonFunctionHelperContent(number::lowerThanOrEqual);
+            }
+        }
+    }
+}
+
+#undef LCPP_lambda_builtinFunctions_defineComparisonFunctionHelperContent
--- source/lcppCoreCont/lcpp/core/builtins/lambda_builtinFunctions.h
+++ source/lcppCoreCont/lcpp/core/builtins/lambda_builtinFunctions.h
@@ -9,11 +9,15 @@ namespace lcpp
     {
         namespace builtin
         {
+            // Arithmetic
+            //////////////////////////////////////////////////////////////////////////
             LCPP_API_CORE_CONT Ptr<LispObject> add(Ptr<LispObject> pCont);
             LCPP_API_CORE_CONT Ptr<LispObject> subtract(Ptr<LispObject> pCont);
             LCPP_API_CORE_CONT Ptr<LispObject> multiply(Ptr<LispObject> pCont);
             LCPP_API_CORE_CONT Ptr<LispObject> divide(Ptr<LispObject> pCont);
 
+            // Comparison
+            //////////////////////////////////////////////////////////////////////////
             LCPP_API_CORE_CONT Ptr<LispObject> greaterThan(Ptr<LispObject> pCont);
             LCPP_API_CORE_CONT Ptr<LispObject> greaterThanOrEqual(Ptr<LispObject> pCont);
             LCPP_API_CORE_CONT Ptr<LispObject> equal(Ptr<LispObject> pCont);
