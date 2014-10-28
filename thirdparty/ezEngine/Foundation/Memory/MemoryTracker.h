463fabcba5d66f8b76abf6d130a7d177742221da
Manuel Maier <mjmaier@gmx.de>
2014-10-28 23:23:59 +0100

    Added patched ez memory tracker by benni

--- thirdparty/ezEngine/Foundation/Memory/MemoryTracker.h
+++ thirdparty/ezEngine/Foundation/Memory/MemoryTracker.h
@@ -16,7 +16,7 @@ struct ezMemoryTrackingFlags
     EnableStackTrace = EZ_BIT(1),
 
     All = EnableTracking | EnableStackTrace,
-
+    //Default = 0
     Default = EnableTracking
 #if EZ_ENABLED(EZ_USE_ALLOCATION_STACK_TRACING)
     | EnableStackTrace
@@ -42,10 +42,27 @@ public:
   {
     EZ_DECLARE_POD_TYPE();
 
-    size_t m_uiSize;
-    size_t m_uiAlignment;
+    void** m_pStackTrace;
+    ezUInt32 m_uiSize;
+    ezUInt16 m_uiAlignment;
+    ezUInt16 m_uiStackTraceLength;
 
-    ezArrayPtr<void*> m_StackTrace;
+    EZ_FORCE_INLINE const ezArrayPtr<void*> GetStackTrace() const
+    {
+      return ezArrayPtr<void*>(m_pStackTrace, (ezUInt32)m_uiStackTraceLength);
+    }
+
+    EZ_FORCE_INLINE ezArrayPtr<void*> GetStackTrace()
+    {
+      return ezArrayPtr<void*>(m_pStackTrace, (ezUInt32)m_uiStackTraceLength);
+    }
+
+    EZ_FORCE_INLINE  void SetStackTrace(ezArrayPtr<void*> stackTrace)
+    {
+      m_pStackTrace = stackTrace.GetPtr();
+      EZ_ASSERT(stackTrace.GetCount() < 0xFFFF, "stack trace to long");
+      m_uiStackTraceLength = (ezUInt16)stackTrace.GetCount();
+    }
   };
 
   class EZ_FOUNDATION_DLL Iterator
@@ -74,6 +91,7 @@ public:
 
   static void AddAllocation(ezAllocatorId allocatorId, const void* ptr, size_t uiSize, size_t uiAlign);
   static void RemoveAllocation(ezAllocatorId allocatorId, const void* ptr);
+  static void ReplaceAllocation(ezAllocatorId allocatorId, const void* ptr, size_t uiOldSize, size_t uiNewSize);
 
   static const char* GetAllocatorName(ezAllocatorId allocatorId);
   static const ezAllocatorBase::Stats& GetAllocatorStats(ezAllocatorId allocatorId);
